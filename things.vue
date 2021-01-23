6d1640df458737cc5b336a2e62a3738baf09d723
stealth-wave <76632048+stealth-wave@users.noreply.github.com>
2021-01-23 15:53:03 -0500

    a lot happened

---
+++ things.vue
@@ -0,0 +1,17 @@
+js:
+
+function lightmode() {
+  document.documentElement.classList.add("light") // this will enable light mode
+  document.documentElement.classList.remove("light") // this will DISABLE lightmode=
+}
+
+
+css:
+
+.light #Hire {
+  /* css for #Hire in light mode */
+}
+
+#Hire {
+  /* css for #Hire in not light mode */
+}
